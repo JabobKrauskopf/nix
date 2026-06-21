628d946b18426d721b873322ce9a8dff089a5fec
Jakob Kraus <mail@jakobkraus.de>
2026-06-21 14:05:14 +0200

    add spotify and proton vpn

--- common/desktops/main.nix
+++ common/desktops/main.nix
@@ -11,11 +11,13 @@
     ../programs/programming.nix
     ../programs/uxplay.nix
     ../programs/jellyfin.nix
+    ../programs/spotify.nix
   ];
 
   environment.systemPackages = with pkgs; [
     teamspeak6-client
     element-desktop
     slack
+    proton-vpn
   ];
 }
